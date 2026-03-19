e8ad2436f9f7e7f8b2bc6d743fc1f652a800fa8f
taka <t.urakami1979@gmail.com>
2026-03-19 19:51:04 +0900

    v74 speed up

--- autonomy/v74/esde_v74_engine.py
+++ autonomy/v74/esde_v74_engine.py
@@ -31,6 +31,7 @@ for p in [str(_SCRIPT_DIR), str(_V43_DIR), str(_V41_DIR), str(_ENGINE_DIR)]:
 
 import engine_accel
 assert hasattr(engine_accel, '_fast_link_strength_sum')
+import engine_accel_v3  # v3: key(), get_latent() speedup
 
 from esde_v43_engine import (
     V43Engine, EncapsulationParams, V43StateFrame,
